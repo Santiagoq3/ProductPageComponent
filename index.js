ec54387a60e6fdd5f8d607b3069edba121ae1cae
Santiago Quipildor <Quipildorsantiago@gmail.com>
2021-10-14 16:21:34 -0300

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 
 const body = document.getElementsByTagName("body")[0];
 const cart = document.querySelector(".cart_display")
-
+const demos = document.querySelectorAll(".demos")
+const imgSlide = document.querySelector(".imgSlide")
 const asideNavCategories = document.querySelector(".aside-nav_categories")
-
 const btnOpenAsideNav = document.querySelector(".nav_aside_btn-open")
-
 const btnCloseAsideNav = document.querySelector(".nav-aside_btn-close");
-
 const btnOpenCartDisplay = document.querySelector(".cart")
+const contadorHtml = document.querySelector(".contadorhtml")
+const btnSumar = document.querySelector(".sumar")
+const btnRestar = document.querySelector(".restar")
+const btnAddCart = document.querySelector(".cart_actions-add-product")
+const btnDeleteProductCard = document.querySelector(".cart_products-icon-delete")
+
+let contador = 0;
+
+
+demos.forEach(demo=>{
+    demo.addEventListener("click", function(){
+        if(demo.classList.contains("demo1")){
+            imgSlide.src ="assets/images/image-product-1.jpg"
+        }
+        if(demo.classList.contains("demo2")){
+            imgSlide.src ="assets/images/image-product-2.jpg"
+        }
+        if(demo.classList.contains("demo3")){
+            imgSlide.src ="assets/images/image-product-3.jpg"
+        }
+        if(demo.classList.contains("demo4")){
+            imgSlide.src ="assets/images/image-product-4.jpg"
+        }
+        
+    })
+})
+
+
 
 btnOpenAsideNav.addEventListener("click", function(){
     asideNavCategories.style.display = "block";
@@ -29,5 +55,53 @@ btnOpenCartDisplay.addEventListener("click", function(){
         cart.style.display = "none"
     }
 })
+
+btnSumar.addEventListener("click", function(){
+    contador= contador + 1;
+
+   contadorHtml.textContent = contador
+})
+btnRestar.addEventListener("click", function(){
+
+    contador= contador - 1;
+    if(contador < 0){
+        contador = 0
+    }
+    
+
+    contadorHtml.textContent = contador
+})
+
+btnAddCart.addEventListener("click", function(){
+
+    const product = document.createElement("div")
+
+    product.classList.add("productCart")
+
+    product.innerHTML = `
+    <img class="cart_products-img" src="assets/images/image-product-1.jpg" alt="">
+    <span class="cart_products-title">Fall Liteded Edition</span>
+    <span class="cart_products-price">$125.00</span>
+    <b></b>
+    <img class="cart_products-icon-delete" src="assets/images/icon-delete.svg" alt="" onclick = deleteProduct()>
+
+    
+    `
+
+    document.querySelector(".cart_products").appendChild(product)
+})
+
+function deleteProduct(){
+    document.querySelector(".productCart").remove()
+
+}
+
+
+
+
+
+
+
+
 
 
